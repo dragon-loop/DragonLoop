391e961e8c17afa602d5f81513c583033cb52290
Ryan Canavan <rycanavan@gmail.com>
2019-07-25 21:11:39 -0400

    Initial commit of React Native app and reorganized repo

---
+++ DrexelBusApp/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>This is the home screen.</Text>
+    </View>
+  );
+};
+
+export default App;
