b9d7ac25d64d471a36289b1f854981c1977fda5e
Ryan Canavan <rycanavan@gmail.com>
2019-10-02 18:57:27 -0400

    Create schedules page and cleanup duplicate services

---
+++ DragonLoop/DragonLoopApp/DragonLoopApp/Views/SchedulesPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using DragonLoopApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DragonLoopApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SchedulesPage : ContentPage
+    {
+        SchedulesViewModel viewModel;
+
+        public SchedulesPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new SchedulesViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Schedules == null || !viewModel.Schedules.Any())                
+                viewModel.LoadSchedulesCommand.Execute(null);
+        }
+    }
+}
