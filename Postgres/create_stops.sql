2a8809335f67a5e52ecd5d6169f8bb12a099bc58
Ryan Canavan <rycanavan@gmail.com>
2019-10-03 21:23:53 -0400

    Added sequences to create tables scripts and added order to run scripts in README

--- Postgres/create_stops.sql
+++ Postgres/create_stops.sql
@@ -1,3 +1,17 @@
+-- SEQUENCE: public.stop_stop_id_seq
+
+-- DROP SEQUENCE public.stop_stop_id_seq;
+
+CREATE SEQUENCE public.stop_stop_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+ALTER SEQUENCE public.stop_stop_id_seq
+    OWNER TO postgres;
+
 -- Table: public.stops
 
 -- DROP TABLE public.stops;
