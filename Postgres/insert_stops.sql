39b9993efff0d6c9557aaac9aef18db65ee2046b
Ryan Canavan <rycanavan@gmail.com>
2019-10-26 13:24:04 -0400

    Add route_segments DB table

--- Postgres/insert_stops.sql
+++ Postgres/insert_stops.sql
@@ -10,14 +10,3 @@ INSERT INTO public.stops (x_coordinate, y_coordinate, name, route_id) VALUES
 (39.955619, -75.189475,'33rd & Market Streets',2),
 (39.956458, -75.164472,'15th and Race Streets',3),
 (40.019915, -75.181201,'Queen Lane',3);
-UPDATE public."Stops" SET next_stop_id = 2 WHERE stop_id = 1;
-UPDATE public."Stops" SET next_stop_id = 3 WHERE stop_id = 2;
-UPDATE public."Stops" SET next_stop_id = 4 WHERE stop_id = 3;
-UPDATE public."Stops" SET next_stop_id = 1 WHERE stop_id = 4;
-UPDATE public."Stops" SET next_stop_id = 6 WHERE stop_id = 5;
-UPDATE public."Stops" SET next_stop_id = 7 WHERE stop_id = 6;
-UPDATE public."Stops" SET next_stop_id = 8 WHERE stop_id = 7;
-UPDATE public."Stops" SET next_stop_id = 9 WHERE stop_id = 8;
-UPDATE public."Stops" SET next_stop_id = 5 WHERE stop_id = 9;
-UPDATE public."Stops" SET next_stop_id = 11 WHERE stop_id = 10;
-UPDATE public."Stops" SET next_stop_id = 10 WHERE stop_id = 11;
