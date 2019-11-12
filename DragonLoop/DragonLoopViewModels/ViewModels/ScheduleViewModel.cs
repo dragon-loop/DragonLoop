06203b8ebb54e3346e92bbdc6e3fe3949665af29
Ryan Canavan <rycanavan@gmail.com>
2019-11-11 23:52:19 -0500

    Major API/service refactoring + map and schedule page improvements

--- DragonLoop/DragonLoopViewModels/ViewModels/ScheduleViewModel.cs
+++ DragonLoop/DragonLoopViewModels/ViewModels/ScheduleViewModel.cs
@@ -8,39 +8,31 @@ namespace DragonLoopViewModels.ViewModels
 {
     public class ScheduleViewModel
     {
+        private RouteService RouteService;
+
         public IEnumerable<IGrouping<int, Schedule>> Schedules { get; set; }
 
         public IEnumerable<Route> Routes { get; set; }
 
         public Route SelectedRoute { get; private set; }
 
-        private ScheduleService ScheduleService;
-
-        private RouteService RouteService;
-
-        private StopService StopService;
-
         public ScheduleViewModel(string urlBase)
         {
-            ScheduleService = new ScheduleService(urlBase);
             RouteService = new RouteService(urlBase);
-            StopService = new StopService(urlBase);
         }
 
         public async Task LoadRoutes()
             => Routes = await RouteService.GetRoutesAsync();
 
-        public async Task LoadSchedule()
-        {
-            var schedules = await ScheduleService.GetSchedulesAsync(SelectedRoute.RouteId);
-            Schedules = schedules.GroupBy(s => s.TripId);
-        }
-
         public async Task SetSelectedRoute(Route route)
         {
             SelectedRoute = route;
-            var stops = await StopService.GetStopsAsync(SelectedRoute.RouteId);
+
+            var stops = await RouteService.GetStopsAsync(SelectedRoute.RouteId);
             SelectedRoute.Stops = stops.ToList();
-        }
+
+            var schedules = await RouteService.GetSchedulesAsync(SelectedRoute.RouteId);
+            Schedules = schedules.OrderBy(s => s.ExpectedTime).GroupBy(s => s.TripId);
+        }       
     }
 }
