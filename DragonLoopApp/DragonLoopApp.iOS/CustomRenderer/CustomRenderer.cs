8d6c2915f3bcaa1becebca4e5b2d2f02ec349d2e
Ryan Canavan <rycanavan@gmail.com>
2020-02-08 16:42:32 -0500

    Fix usings

--- DragonLoopApp/DragonLoopApp.iOS/CustomRenderer/CustomRenderer.cs
+++ DragonLoopApp/DragonLoopApp.iOS/CustomRenderer/CustomRenderer.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using CoreGraphics;
 using DragonLoopApp.iOS.CustomRenderer;
-using DragonLoopApp.Views;
-using Foundation;
+using DragonLoopApp.Views.MapElements;
 using MapKit;
 using UIKit;
 using Xamarin.Forms;
