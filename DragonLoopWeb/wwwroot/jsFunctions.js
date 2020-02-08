399d714b811810c1615f1f2b594a16e8c77fc4b1
shi24 <shi24@drexel.edu>
2020-02-07 21:03:23 -0500

    Added Stops toggle for Web

--- DragonLoopWeb/wwwroot/jsFunctions.js
+++ DragonLoopWeb/wwwroot/jsFunctions.js
@@ -1,8 +1,30 @@
 ﻿window.jsFunctions = {
     initMap: function () {
+        map = jsFunctions.getMap();
+    },
+    getMap: function () {
         var map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: 39.955615, lng: -75.189490 },
             zoom: 14
         });
+
+        return map;
+    },
+    setMarker: function (posX, postY, name) {
+        var marker = new google.maps.Marker({
+            position: { lat: posX, lng: postY },
+            map: map,
+            animation: google.maps.Animation.DROP,
+            title: name
+        });
+
+        marker.addListener('click', function () {
+            if (marker.getAnimation() !== null) {
+                marker.setAnimation(null);
+            } else {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        });
+
     }
 }
